7799eb28826e27e66868b7be806b7879af0b2b9c
the21st <the21st@7c4e3229-ff4e-0410-878e-0b87fffd5073>
2008-06-06 13:13:20 +0000

    first commit

---
+++ trunk/Physics/PhysicsObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Physics
+{
+    public abstract class PhysicsObject
+    {
+        protected World world;
+
+        public abstract void Tick( float time );
+
+        public abstract void Render();
+    }
+}
---
+++ trunk/Physics/Vector.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Physics
+{
+    public class Vector
+    {
+        public float x, y;
+
+        public Vector()
+        {
+            x = y = 0f;
+        }
+
+        public Vector( int x, int y )
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Vector( float x, float y )
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public float Abs()
+        {
+            return (float)Math.Sqrt( x * x + y * y );
+        }
+
+        public Vector Normalized()
+        {
+            return this / this.Abs();
+        }
+
+        public static Vector Projection( Vector v, Vector target )
+        {
+            return ((v * target) / (target.Abs() * target.Abs())) * target;
+        }
+
+        public static float ProjectionLength( Vector v, Vector target )
+        {
+            return (v * target) / target.Abs();
+        }
+
+        public static Vector operator +( Vector v1, Vector v2 )
+        {
+            return new Vector( v1.x + v2.x, v1.y + v2.y );
+        }
+
+        public static Vector operator -( Vector v1, Vector v2 )
+        {
+            return new Vector( v1.x - v2.x, v1.y - v2.y );
+        }
+
+        public static Vector operator *( float a, Vector v )
+        {
+            return new Vector( a * v.x, a * v.y );
+        }
+
+        public static Vector operator *( Vector v, float a )
+        {
+            return new Vector( a * v.x, a * v.y );
+        }
+
+        public static float operator *( Vector v1, Vector v2 )
+        {
+            return v1.x * v2.x + v1.y * v2.y;
+        }
+
+        public static Vector operator /( Vector v, float a )
+        {
+            return new Vector( v.x / a, v.y / a );
+        }
+
+        public override string ToString()
+        {
+            return this.x + ", " + this.y;
+            //return "(" + this.x + ", " + this.y + ")";
+        }
+    }
+}
