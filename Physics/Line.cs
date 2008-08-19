01bb6e16abee9423de5363eb00b2b2897cc3c2f2
the21st <the21st@7c4e3229-ff4e-0410-878e-0b87fffd5073>
2008-08-19 18:51:56 +0000

    idem menit riesenie detekcie kolizie sphere a board

---
+++ Physics/Line.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Physics
+{
+    public class Line
+    {
+        public Vector Start, End;
+
+        public Line()
+        {
+            Start = new Vector();
+            End = new Vector();
+        }
+
+        public Line( Vector start, Vector end )
+        {
+            Start = start;
+            End = end;
+        }
+
+        public Line( float x1, float y1, float x2, float y2 )
+        {
+            Start = new Vector( x1, y1 );
+            End = new Vector( x2, y2 );
+        }
+
+        public Line( Line copy )
+        {
+            Start = new Vector( copy.Start.x, copy.Start.y );
+            End = new Vector( copy.End.x, copy.End.y );
+        }
+    }
+}
