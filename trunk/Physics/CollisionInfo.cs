c28a98b8e1111fb3763c1e18725f9d987296775b
the21st <the21st@7c4e3229-ff4e-0410-878e-0b87fffd5073>
2008-08-31 16:41:16 +0000

    aj s playgroundom

---
+++ trunk/Physics/CollisionInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Physics
+{
+    public class CollisionInfo
+    {
+        public Vector Location;
+        public Board With;
+        public byte Type;
+
+        public CollisionInfo( Vector location, Board with, byte type )
+        {
+            Location = location;
+            With = with;
+            Type = type;
+        }
+    }
+}
